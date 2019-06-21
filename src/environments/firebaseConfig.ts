873dc72e771a31cf9ca3d2899d7a04d84453d00f
Yashodhan <you@example.com>
2019-06-21 14:30:57 +0530

    first commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBhRseRnm3tudQ8cG_Kxr2gWOn5YM7EUn8",
+  authDomain: "pm-demo-429e7.firebaseapp.com",
+  databaseURL: "https://pm-demo-429e7.firebaseio.com",
+  projectId: "pm-demo-429e7",
+  storageBucket: "",
+  messagingSenderId: "1097272555042",
+  appId: "1:1097272555042:web:cef5b87fed5bbc96"
+};
